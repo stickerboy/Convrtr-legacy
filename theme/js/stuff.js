47b32c52a31ef3e90619cb14f5ac13cbe1a530af
Kenny <git@kennycameron.co.uk>
2013-06-15 19:58:48 +0100

    A little show/hide functionality

--- theme/js/stuff.js
+++ theme/js/stuff.js
@@ -1,3 +1,22 @@
-$(".selectAll").click(function(){
-	$(this).closest('.convrt-box').find('textarea').select();
+   
+$(document).ready(function() {
+
+    $('.selectAll').click(function(){
+        $(this).closest('.convrt-box').find('textarea').select();
+    });
+
+    $(".sh").click(function(e){
+        e.preventDefault(); 
+        var thish = $(this);
+        var ccs = $(this).closest('.convrt-box').find('.shh');					
+    
+        $(ccs).toggle(function() {
+            if ($(this).css('display')=='none') {
+                $(thish).text('Show');
+            }
+            else {
+                $(thish).text('Hide');
+            }
+        });        
+    });
 });
